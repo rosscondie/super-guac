936e0a618bca1aab9b12a276693da11625774e2d
Ross Condie <rosscondie@gmail.com>
2025-07-18 11:39:26 +0200

    fix CORS issue for creating album

--- main.go
+++ main.go
@@ -20,8 +20,10 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: os.Getenv("FRONTEND_ORIGIN"),
-		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowOrigins:     os.Getenv("FRONTEND_ORIGIN"),
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowCredentials: true,
 	}))
 
 	app.Static("/", "./public")
